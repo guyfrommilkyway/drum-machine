fa95f8e52a420d9d5b54b46235327eb97b5f473c
Almer Tampus <almerflorestampus@gmail.com>
2023-08-07 01:16:15 +0800

    Add instruments data

---
+++ data/instruments.js
@@ -0,0 +1,47 @@
+export const instruments = [
+  {
+    instrument: 'heater-1',
+    id: 'Q',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+  },
+  {
+    instrument: 'heater-2',
+    id: 'W',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+  },
+  {
+    instrument: 'heater-3',
+    id: 'E',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+  },
+  {
+    instrument: 'heater-4',
+    id: 'A',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+  },
+  {
+    instrument: 'clap',
+    id: 'S',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+  },
+  {
+    instrument: 'open-hh',
+    id: 'D',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+  },
+  {
+    instrument: 'kick-n-hat',
+    id: 'Z',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+  },
+  {
+    instrument: 'kick',
+    id: 'X',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+  },
+  {
+    instrument: 'closed-hh',
+    id: 'C',
+    audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+  },
+];
