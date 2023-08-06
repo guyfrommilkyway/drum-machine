4bef472ce661a014e722f746094fa46c3f51ad08
Almer Tampus <almerflorestampus@gmail.com>
2023-08-07 01:19:18 +0800

    Integrate Context, Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+// import packages below
+import { ChakraProvider } from '@chakra-ui/react';
+
+// import utils below
+import { MachineContextProvider } from '@/store/machine-context';
+
+// import assets below
+import '../styles/global.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider>
+        <MachineContextProvider>
+          <Component {...pageProps} />
+        </MachineContextProvider>
+      </ChakraProvider>
+      <script src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js'></script>
+    </>
+  );
 }
