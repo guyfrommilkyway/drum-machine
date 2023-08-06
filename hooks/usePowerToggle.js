30f256217451449d621b4b18b8e61794ced67b38
Almer Tampus <almerflorestampus@gmail.com>
2023-08-07 01:16:43 +0800

    Add power functionality

---
+++ hooks/usePowerToggle.js
@@ -0,0 +1,15 @@
+// import packages below
+import { useState, useCallback } from 'react';
+
+const usePowerToggle = () => {
+  const [power, setPower] = useState(true);
+
+  const powerHandler = useCallback(
+    () => setPower((prevState) => !prevState),
+    []
+  );
+
+  return { power, powerHandler };
+};
+
+export default usePowerToggle;
